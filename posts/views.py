d571e668b7aeba4980223204550110210c4a1331
rafabrasail <rafaelrosenberg89@gmail.com>
2021-11-07 19:06:08 -0300

    replit

--- posts/views.py
+++ posts/views.py
@@ -9,7 +9,11 @@ def post_list_and_create(request):
     return render(request, 'posts/main.html', {'qs':qs})
 
 
-def load_post_data_view(request):
+def load_post_data_view(request, num_posts):
+    visible = 1
+    upper = num_posts
+    lower = upper - visible
+    size = Post.objects.all().count()
     qs = Post.objects.all()
     # data = serializers.serialize('json', qs)
     data = []
@@ -17,10 +21,11 @@ def load_post_data_view(request):
         item = {
         'id': obj.id,
         'title': obj.body,
+        'liked': True if request.user in obj.all() else False,
         'author': obj.author.user.username
         }
         data.append(item)
-    return JsonResponse({'data': data})
+    return JsonResponse({'data': data[lower:upper], 'size': size})
 
 
 def hello_world_view(request):
--- static/js/main.js
+++ static/js/main.js
@@ -5,55 +5,72 @@ const postsBox = document.getElementById('posts-box')
 const spinnerBox = document.getElementById('spinner-box')
 const spinnerBoxx = document.getElementById('spinner-boxx')
 
+const loadBtn = document.getElementById('load-btn')
+const endBox = document.getElementById('end-box')
+
 // helloWorld.textContent = 'hello world'
 // helloWorld.innerHTML = 'hello <b>world</b>'
 
-$.ajax({
-    type: 'GET',
-    url: '/hello/',
-    success: function(response){
-        console.log('success', response.text)
-        console.log('success', response)
-        helloWorld.innerHTML = 'hello <b>world</b>'
-    },
-    error: function(error){
-        console.log('error', error)
-    }
-})
+// $.ajax({
+//     type: 'GET',
+//     url: '/hello/',
+//     success: function(response){
+//         console.log('success', response.text)
+//         console.log('success', response)
+//         helloWorld.innerHTML = 'hello <b>world</b>'
+//     },
+//     error: function(error){
+//         console.log('error', error)
+//     }
+// })
 
-$.ajax({
-    type: 'GET',
-    url: '/data/',
-    success: function(response){
-        console.log(response)
-        const data = response.data
-        setTimeout( () => {
-            spinnerBox.classList.add('not-visible')
-            spinnerBoxx.classList.add('not-visible')
-            console.log(data)
-            data.forEach(el => {
-                postsBox.innerHTML += `
-                    <div class="card mb-2">
-                        <div class="card-body">
-                            <h5 class="card-title">${el.title}</h5>
-                            <p class="card-text">${el.body}</p>
-                        </div>
-                        <div class="card-footer">   
-                            <div class="row">
-                                <div class="col">
-                                    <a href="#" class="btn btn-primary">Details</a>
-                                </div>
-                                <div class="col">
-                                    <a href="#" class="btn btn-primary">Like</a>
+let visible = 1
+
+
+const getData = () => {
+    $.ajax({
+        type: 'GET',
+        url: `/data/${visible}/`,
+        success: function(response){
+            console.log(response)
+            const data = response.data
+            setTimeout( () => {
+                spinnerBox.classList.add('not-visible')
+                spinnerBoxx.classList.add('not-visible')
+                console.log(data)
+                data.forEach(el => {
+                    postsBox.innerHTML += `
+                        <div class="card mb-2">
+                            <div class="card-body">
+                                <h5 class="card-title">${el.title}</h5>
+                                <p class="card-text">${el.body}</p>
+                            </div>
+                            <div class="card-footer">   
+                                <div class="row">
+                                    <div class="col">
+                                        <a href="#" class="btn btn-primary">Details</a>
+                                    </div>
+                                    <div class="col">
+                                        <a href="#" class="btn btn-primary">Like</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                `
-            });
-        }, 1000)
-    },
-    error: function(error){
-        console.log(error)
-    }
+                    `
+                });
+            }, 100)
+            console.log(response.size)
+        },
+        error: function(error){
+            console.log(error)
+        }
+    })
+}
+
+loadBtn.addEventListener('click', () => {
+    spinnerBox.classList.remove('not-visible')
+    visible += 3
+    getData()
 })
+
+getData()
